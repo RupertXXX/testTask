89557a59cb4929ee3a09feba54d2877560038773
RupertXXX <nicolaissssm@gmail.com>
2021-04-29 20:10:10 +0300

    changed language in validations errors

--- src/common/validators/validators.js
+++ src/common/validators/validators.js
@@ -1,18 +1,18 @@
 export const required = (value) => {
-    if (!value) return 'This field is required';
+    if (!value) return 'Это поле обязательно';
     else return undefined;
 }
 
 export const maxLengthCreator = (length) => {
     return (value) => {
-        if(value && value.length > length) return `Max length ${length}`;
+        if(value && value.length > length) return `Максимальная длина ${length}`;
         else return undefined;
     }
 }
 
 export const minLengthCreator = (length) => {
     return (value) => {
-        if(value && value.length < length) return `Min length ${length}`;
+        if(value && value.length < length) return `Минимальная длина ${length}`;
         else return undefined;
     }
 }
