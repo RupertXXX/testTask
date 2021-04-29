445b31da07631bf4b98f8557368b90173f86693c
RupertXXX <nicolaissssm@gmail.com>
2021-04-29 19:16:14 +0300

    finish

--- src/common/formControls/formControls.jsx
+++ src/common/formControls/formControls.jsx
@@ -1,79 +1,72 @@
 import React from 'react';
 import c from './formControls.module.css';
 
+const InputConstructor = ({meta, ...props}) => {
+    return<>
+        {props.children}
+        <div className={ 
+            (((meta.touched && !meta.active) || meta.submitFailed) && meta.error) 
+                ? c.error_input_text_message 
+                : c.error_input_text_message_none 
+            } >
+            {(((meta.touched && !meta.active) || meta.submitFailed) && meta.error) && <span>{meta.error}</span>}
+        </div>
+    </>
+}
+
 export const InputText = ({input, meta, ...props}) => {
     return(
         <>
-            <input type="text" className={ 
-                    (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
-                        ? c.input_text_error 
-                        : c.input_text 
-                } 
-            {...input} {...props} />
-            <div className={ 
-                (((meta.touched && !meta.active) || meta.submitFailed) && meta.error) 
-                    ? c.error_input_text_message 
-                    : c.error_input_text_message_none 
-                } >
-                {(((meta.touched && !meta.active) || meta.submitFailed) && meta.error) && <span>{meta.error}</span>}
-            </div>
+            <InputConstructor meta={meta}>
+                <input type="text" className={ 
+                        (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
+                            ? c.input_text_error 
+                            : c.input_text 
+                    } 
+                {...input} {...props} />
+            </InputConstructor>
         </>
     )
 }
 export const InputEmail = ({input, meta, ...props}) => {
     return(
         <>
-            <input type="email" className={ 
-                    (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
-                        ? c.input_text_error 
-                        : c.input_text 
-                } 
-            {...input} {...props} />
-            <div className={ 
-                (((meta.touched && !meta.active) || meta.submitFailed) && meta.error) 
-                    ? c.error_input_text_message 
-                    : c.error_input_text_message_none 
-                } >
-                {(((meta.touched && !meta.active) || meta.submitFailed) && meta.error) && <span>{meta.error}</span>}
-            </div>
+            <InputConstructor meta={meta}>
+                <input type="email" className={ 
+                        (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
+                            ? c.input_text_error 
+                            : c.input_text 
+                    } 
+                {...input} {...props} />
+            </InputConstructor>
         </>
     )
 }
 export const InputPassword = ({input, meta, ...props}) => {
     return(
         <>
-            <input type="password" className={ 
-                    (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
-                        ? c.input_text_error 
-                        : c.input_text 
-                } 
-            {...input} {...props} />
-            <div className={ 
-                (((meta.touched && !meta.active) || meta.submitFailed) && meta.error) 
-                    ? c.error_input_text_message 
-                    : c.error_input_text_message_none 
-                } >
-                {(((meta.touched && !meta.active) || meta.submitFailed) && meta.error) && <span>{meta.error}</span>}
-            </div>
+            <InputConstructor meta={meta}>
+                <input type="password" className={ 
+                        (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
+                            ? c.input_text_error 
+                            : c.input_text 
+                    } 
+                {...input} {...props} />
+            </InputConstructor>
         </>
     )
 }
 export const InputDate = ({input, meta, ...props}) => {
     return(
         <>
-            <input type="date" className={ 
-                    (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
-                        ? c.input_date_error 
-                        : c.input_date 
-                } 
-            {...input} {...props} />
-            <div className={ 
-                (((meta.touched && !meta.active) || meta.submitFailed) && meta.error) 
-                    ? c.error_input_text_message 
-                    : c.error_input_text_message_none 
-                } >
-                {(((meta.touched && !meta.active) || meta.submitFailed) && meta.error) && <span>{meta.error}</span>}
-            </div>
+            <InputConstructor meta={meta}>
+                <input type="date" className={ 
+                        (((meta.touched && !meta.active) || meta.submitFailed) && meta.error)
+                            ? c.input_text_error 
+                            : c.input_text 
+                    } 
+                {...input} {...props} />
+            </InputConstructor>
         </>
     )
 }
